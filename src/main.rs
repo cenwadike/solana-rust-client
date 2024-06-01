71f90faad2239b990a597c59b31edbb972bb1e9c
cenwadike <chinedunwadike04@gmail.com>
2024-06-01 09:30:15 +0100

    clean up

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use blob;
 use anchor_client;
+use blob;
 use borsh::{BorshDeserialize, BorshSerialize};
 pub use solana_client::rpc_client::RpcClient;
 pub use solana_sdk::{
@@ -8,15 +8,8 @@ pub use solana_sdk::{
     signature::{Keypair, Signer},
     system_program,
     transaction::Transaction,
-    transaction_context,
 };
 use solana_sdk::{message::Message, signature::Signature, signer::EncodableKey};
-
-
-// function signatures
-const INITIALIZE_DISCRIMINANT: [u8; 8] = [175, 175, 109, 31, 13, 152, 155, 237];
-const UPDATE_DISCRIMINANT: [u8; 8] = [18, 162, 49, 169, 237, 193, 10, 33];
-
 
 #[derive(BorshSerialize, BorshDeserialize)]
 #[borsh(crate = "borsh")]
@@ -28,7 +21,6 @@ pub struct UpdateBlob {
     pub data: Vec<u8>,
 }
 
-
 pub fn initialize_program() -> Result<Signature, Box<dyn std::error::Error>> {
     let rpc_connection = RpcClient::new("https://api.devnet.solana.com");
     let program_id = blob::ID;
@@ -37,7 +29,7 @@ pub fn initialize_program() -> Result<Signature, Box<dyn std::error::Error>> {
 
     let payer: Keypair =
         Keypair::read_from_file("/Users/cenwadike/.config/solana/solfate-dev.json")?;
-    
+
     println!("program id: {:?}", program_id);
     println!("payer: {:?}", payer.pubkey());
     println!("blob account: {:?}", blob_account);
@@ -51,10 +43,13 @@ pub fn initialize_program() -> Result<Signature, Box<dyn std::error::Error>> {
         AccountMeta::new(system_program::id(), false),
     ];
 
+    // get initialize discriminant
+    let initialize_discriminant = get_discriminant("global", "initialize");
+
     // construct instruction
     let ix = Instruction::new_with_borsh(
         program_id.clone(),
-        &(INITIALIZE_DISCRIMINANT, instruction_data),
+        &(initialize_discriminant, instruction_data),
         accounts.clone(),
     );
 
@@ -75,11 +70,14 @@ pub fn initialize_program() -> Result<Signature, Box<dyn std::error::Error>> {
     // send and confirm transaction
     let tx_signature = rpc_connection.send_and_confirm_transaction(&tx)?;
 
-    println!("Initialization successful. Tx signature: {:?}", tx_signature);
+    println!(
+        "Initialization successful. Tx signature: {:?}",
+        tx_signature
+    );
     Ok(tx_signature)
 }
 
-pub fn update() -> Result<Signature, Box<dyn std::error::Error>> {
+pub fn update_blob() -> Result<Signature, Box<dyn std::error::Error>> {
     let rpc_connection = RpcClient::new("https://api.devnet.solana.com");
     let program_id = blob::ID;
 
@@ -104,17 +102,18 @@ pub fn update() -> Result<Signature, Box<dyn std::error::Error>> {
         AccountMeta::new(system_program::id(), false),
     ];
 
+    // get discriminant
+    let update_blob_discriminant = get_discriminant("global", "update_blob");
+
     // construct instruction
     let ix = Instruction::new_with_borsh(
         program_id.clone(),
-        &(UPDATE_DISCRIMINANT, instruction_data),
+        &(update_blob_discriminant, instruction_data),
         accounts.clone(),
     );
 
     // get latest block hash
-    let blockhash = rpc_connection
-        .get_latest_blockhash()
-        .expect("latest blockhash");
+    let blockhash = rpc_connection.get_latest_blockhash()?;
 
     // construct message
     let msg = Message::new_with_blockhash(&[ix], Some(&payer.pubkey()), &blockhash);
@@ -144,11 +143,6 @@ pub fn get_discriminant(namespace: &str, name: &str) -> [u8; 8] {
 }
 
 fn main() {
-    println!("Hello, world!");
-
-    initialize_program().expect("error");
-
-    update().expect("error");
-
-    println!("{:?}", get_discriminant("global", "update_blob"));
+    // initialize_program().expect("error");
+    update_blob().expect("error");
 }
